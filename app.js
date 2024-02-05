9e5ed44a131a9a8f9fb65c99d3b868c8f90df490
Khalid  Jameer <khalidjameer@Khalids-Air.lan>
2024-02-05 16:01:55 -0500

    fix: added cors headers

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(cors({
   origin: '*'
 }));
 
+app.use((req, res, next) => {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  next();
+});
+
+
 //create controller
 
 app.use("/users", usersController)
