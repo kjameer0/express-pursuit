68d3f69f93525b530b88a169f45c3494617b7b8a
Khalid  Jameer <khalidjameer@Khalids-Air.lan>
2024-02-05 16:05:24 -0500

    fix: reordered middleware

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
-const usersController = require("./usersController")
-const cors = require("cors")
-app.get("/", (request, response) => {
-  response.send("This is an app my guy.")
-})
+const usersController = require("./usersController");
+const cors = require("cors");
 
-app.use(cors({
-  origin: '*'
-}));
+app.get("/", (request, response) => {
+  response.send("This is an app my guy.");
+});
+
+app.use("/users", usersController);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use((req, res, next) => {
-
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
   next();
 });
 
-
 //create controller
 
-app.use("/users", usersController)
 module.exports = app;
